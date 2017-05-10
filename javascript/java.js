a503e96e6d3e63a11ab1f3db6d5f562d6691293a
Cerys Gibbins <cerys_gibbins@hotmail.co.uk>
2017-05-10 10:21:26 +0100

    Images/JavaScript

---
+++ javascript/java.js
@@ -0,0 +1,23 @@
+﻿        $(document).ready(function () {
+            $("#navHamburger > a").click(function () {
+                if ($("body").hasClass("toggleNavigationVisible") == false) {
+                    $("#navMainMenu").show();
+                    $("#navHamburger").addClass("expand");
+                    $("body").addClass("toggleNavigationVisible");
+					$("#navHamburger span").hide();
+                    return false;
+                }
+            });
+
+            $("#navHamburger").click(function () {
+                if ($("body").hasClass("toggleNavigationVisible") == true) {
+                    $("#navMainMenu").hide();
+                    $("#navHamburger").removeClass("expand");
+                    $("body").removeClass("toggleNavigationVisible");
+					$("#navHamburger span").show();
+                    return false;
+                }
+            });
+        });
+
+   
---
+++ javascript/script.js
@@ -0,0 +1,23 @@
+﻿$(document).ready(function () {
+
+
+
+    // event handler for when the user scrolls the browser window
+$(window).scroll(function () {
+    
+    if ($(this).scrollTop() > 0) {
+        $("#backToTop").show();
+    }
+    else {
+        $("#backToTop").hide();
+    }
+});
+
+$("#backToTop").click(function () {
+    $(window).scrollTop(0);
+        
+    return false;
+});
+
+
+});
